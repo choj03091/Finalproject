7b7db146f77ec1707d96583fc01260c917092af7
cjt0309 <cjtpd0309@naver.com>
2024-11-20 14:46:28 +0900

    FinalprojectApplications.java에서 Apllications.java로 변경

--- Final_Project_Tuesday/src/main/java/com/cjt/tuesday/ServletInitializer.java
+++ Final_Project_Tuesday/src/main/java/com/cjt/tuesday/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FinalProjectTuesdayApplication.class);
+		return application.sources(Application.class);
 	}
 
 }
