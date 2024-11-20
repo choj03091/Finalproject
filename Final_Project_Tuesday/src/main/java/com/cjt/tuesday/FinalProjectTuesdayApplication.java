eb3c990222ad3ba801f2946aec464e216fa9bbd8
cjt0309 <cjtpd0309@naver.com>
2024-11-20 14:29:49 +0900

    Project생성

---
+++ Final_Project_Tuesday/src/main/java/com/cjt/tuesday/FinalProjectTuesdayApplication.java
@@ -0,0 +1,13 @@
+package com.cjt.tuesday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectTuesdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectTuesdayApplication.class, args);
+	}
+
+}
